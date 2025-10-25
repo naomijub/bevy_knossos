4c05cafe15ba5a0296e1fc0770674c2fccfdcb87
Julia Naomi <jnboeira@outlook.com>
2025-10-24 21:52:33 -0400

    test

--- tests/help.rs
+++ tests/help.rs
@@ -42,68 +42,6 @@ Options:
           Print help (see more with '--help')
 ";
 
-const GENERATE_LONG_HELP_STR: &str = "Generates a maze
-
-Usage: bevy_knossos generate [OPTIONS] <COMMAND>
-
-Commands:
-  ascii     Save to a text file with an ASCII representation of a maze
-  game-map  Save to a text file as an ASCII game map for pseudo 3D games that use ray casting for modeling and rendering the map
-  image     Save to PNG or JPG file
-  help      Print this message or the help of the given subcommand(s)
-
-Options:
-  -A, --algorithm <ALGORITHM>
-          Maze generation algorithm
-          
-          [default: recursive-backtracking]
-          [possible values: aldous-broder, binary-tree, eller, growing-tree, hunt-and-kill, kruskal, prim, recursive-backtracking, recursive-division, sidewinder]
-
-  -H, --height <HEIGHT>
-          Grid height in a number of cells
-          
-          [default: 10]
-
-  -S, --seed <SEED>
-          Seed value for deterministic generation (must be a valid u64)
-
-  -W, --width <WIDTH>
-          Grid width in a number of cells
-          
-          [default: 10]
-
-  -C, --start-coords <START_COORDS>
-          Start coordinate for maze algorithm
-
-      --bias[=<BIAS>]
-          Bias to use for the \"Binary Tree\" algorithm
-          
-          [default: north-east]
-
-          Possible values:
-          - north-west: Produces two long corridors on the Northern and Western sides of the maze
-          - north-east: Produces two long corridors on the Northern and Eastern sides of the maze
-          - south-west: Produces two long corridors on the Southern and Western sides of the maze
-          - south-east: Produces two long corridors on the Southern and Eastern sides of the maze
-
-      --growing-method[=<GROWING_METHOD>]
-          Growing method to use for the \"Growing Tree\" algorithm
-          
-          [default: newest]
-
-          Possible values:
-          - newest:            Selects the most recently added cell, thus imitating the recursive backtracker
-          - oldest:            Selects the oldest added cell, thus generating an unchallenging maze with lots of long corridors
-          - random:            Selects cells at random, thus getting Prim's algorithm behaviour
-          - middle:            Selects a middle cell from the list of already added, but produces mazes similar to the ones created by the [Oldest](Method::Oldest) method
-          - newest50-random50: A combination of the [Newest](Method::Newest) and [Random](Method::Random) methods with 50/50 split
-          - newest75-random25: A combination of the [Newest](Method::Newest) and [Random](Method::Random) methods with 75/25 split
-          - newest25-random75: A combination of the [Newest](Method::Newest) and [Random](Method::Random) methods with 25/75 split
-
-  -h, --help
-          Print help (see a summary with '-h')
-";
-
 const GENERATE_IMAGE_HELP_STR: &str = "Save to PNG or JPG file
 
 Usage: bevy_knossos generate image [OPTIONS] --output-path <OUTPUT_PATH>
@@ -155,16 +93,6 @@ fn command_generate_short_help() {
         .assert()
         .success()
         .stdout(GENERATE_SHORT_HELP_STR);
-}
-
-#[test]
-fn command_generate_long_help() {
-    let mut cmd = Command::cargo_bin(assert_cmd::crate_name!()).unwrap();
-    cmd.arg("generate")
-        .arg("--help")
-        .assert()
-        .success()
-        .stdout(GENERATE_LONG_HELP_STR);
 }
 
 #[test]
